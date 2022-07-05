e014ecde6e1f74dea78f1ce1f2d3d6dea2a6d3e4
CJ Chen <chihchieh0906@gmail.com>
2022-07-06 01:02:21 +0800

    refactor: Updated load_data doc_strings

--- bikeshare_3.py
+++ bikeshare_3.py
@@ -65,7 +65,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if applicable.
+    Loads data for the specified city and filters by month and day if data applicable.
     
     Args:
         (str) city - name of the city to analyze
